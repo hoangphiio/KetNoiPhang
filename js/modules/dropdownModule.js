266fee2e2b9cf911919349191c45d08fdec7d80a
Nguyễn Võ Hoàng Phi <phinvh@vuthao.local>
2023-05-12 16:36:00 +0700

    fix

--- js/modules/dropdownModule.js
+++ js/modules/dropdownModule.js
@@ -1,20 +1,23 @@
 export default function dropdownModule() {
   $(document).ready(function () {
-    var hamburger = $(".mobile-menu"), // mobile-menu
+    const hamburger = $(".mobile-menu"),
+      overlay = $(".overlay"), // mobile-menu
       menu = $(".header-link"); //menu
 
     hamburger.on("click", function () {
-      menu.toggleClass("active");
       $(this).toggleClass("active");
-      $(".overlay").toggleClass("active");
+      menu.toggleClass("active");
+      overlay.toggleClass("active");
     });
-    $(".mobile-menu").click(function (e) {
-      $(".menu > ul").toggleClass("show-on-mobile");
-      e.preventDefault();
+    overlay.on("click", function () {
+      $(this).toggleClass("active");
+      menu.toggleClass("active");
+      hamburger.toggleClass("active")
     });
+
     $(".menu > ul > li").click(function () {
       if ($(window).width() <= 1130) {
-        $(this).children("ul").fadeToggle();
+        $(this).children("ul").toggle();
       }
     });
   });
@@ -29,5 +32,4 @@ export default function dropdownModule() {
   $(".rotate").click(function () {
     $(this).toggleClass("down");
   });
-
 }
