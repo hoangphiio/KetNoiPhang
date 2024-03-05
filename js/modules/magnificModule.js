f9730d7cf5d84651d11804e188c4531815885e50
Nguyễn Võ Hoàng Phi <phinvh@vuthao.local>
2024-03-05 17:58:04 +0700

    upload code

--- js/modules/magnificModule.js
+++ js/modules/magnificModule.js
@@ -1,20 +1,25 @@
 export default function magnificModule() {
   jQuery(document).ready(function ($) {
-    $(document).on("click", ".dismiss", function (e) {
-      e.preventDefault();
-      $.magnificPopup.close();
-    });
+    // Kiểm tra xem trang có nút dismiss hay không
+    if ($(".dismiss").length > 0) {
+      $(document).on("click", ".dismiss", function (e) {
+        e.preventDefault();
+        $.magnificPopup.close();
+      });
+    }
 
-    // Youtube
-    $(".popup-youtube").magnificPopup({
-      // disableOn: 700,
-      type: "iframe",
-      mainClass: "mfp-fade",
-      removalDelay: 160,
-      preloader: false,
-      fixedBgPos: true,
-      fixedContentPos: true,
-      fixedContentPos: true,
-    });
+    // Kích hoạt Magnific Popup chỉ trên trang có yêu cầu
+    if ($(".popup-youtube").length > 0) {
+      $(".popup-youtube").magnificPopup({
+        // disableOn: 700,
+        type: "iframe",
+        mainClass: "mfp-fade",
+        removalDelay: 160,
+        preloader: false,
+        fixedBgPos: true,
+        fixedContentPos: true,
+        fixedContentPos: true,
+      });
+    }
   });
 }
--- js/modules/popupModule.js
+++ js/modules/popupModule.js
@@ -1,9 +1,19 @@
 export default function popupModule() {
-  $(".Click-here").on("click", function () {
-    $(".custom-model-main").addClass("model-open");
-  });
-  $(".close-btn, .bg-overlay").click(function () {
-    $(".custom-model-main").removeClass("model-open");
+  function toggleForm($form) {
+    $form.toggleClass("visible");
+    $("body").toggleClass("onscroll");
+  }
+
+  $(".btnOpenForm").on("click", function (event) {
+    event.preventDefault();
+    toggleForm($(".form-popup"));
   });
 
+  $(".form-popup").on("click", function (event) {
+    const $this = $(this);
+    if ($this.is(event.target) || $this.find(".searchClose").is(event.target)) {
+      event.preventDefault();
+      toggleForm($this);
+    }
+  });
 }
